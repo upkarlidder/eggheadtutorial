7b0389ba21dde6e1b43ffd3347462ba79fb35caa
8indaas <8indaas@gmail.com>
2013-04-13 15:18:03 -0700

    added service

--- controller.js
+++ controller.js
@@ -1,7 +1,13 @@
+var myApp = angular.module('myApp',[]);
 
+myApp.factory('Data', function(){
+	return {message:"I am data from a service"};
+});
 
-function FirstCtrl($scope){
+function FirstCtrl($scope, Data){
+	$scope.data = Data;
 }
 
-function SecondCtrl($scope){
+function SecondCtrl($scope, Data){
+	$scope.data = Data;
 }
