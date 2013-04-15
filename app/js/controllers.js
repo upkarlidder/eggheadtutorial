2c4d424237a52a32a34bd13c26db3f6dd536e6ae
8indaas <8indaas@gmail.com>
2013-04-15 11:59:16 -0700

    changed the directory to angular google structure

---
+++ app/js/controllers.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', []).
+  controller('FirstCtrl', [function() {
+  	$scope.data = {Data};
+  }])
+  .controller('SecondCtrl', [function() {
+  	$scope.data = Data;
+  }]);
